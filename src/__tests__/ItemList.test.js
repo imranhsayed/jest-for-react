fa25a7d854eaede3e5d4987f862e4c3dd643f1e2
Imran Sayed <imranhsayed@gmail.com>
2019-03-27 17:55:55 +0530

    Wrote test for ItemList Component

--- src/__tests__/ItemList.test.js
+++ src/__tests__/ItemList.test.js
@@ -3,11 +3,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 test( 'renders "no-items" when the item list is empty', () => {
-	const container = document.createElement( 'div' );
-	ReactDOM.render(
-		<ItemList items={ [] } />,
-		container
-	);
 
-	expect( container.textContent ).toMatch( 'no items' );
+	const container = document.createElement('div');
+	ReactDOM.render(<ItemList items={[]} />, container);
+	expect(container.textContent).toMatch('no items')
 } );
+
+test( 'renders the items in a list', () => {
+	const container = document.createElement('div');
+
+	ReactDOM.render(<ItemList items={['apple', 'orange', 'pear']} />, container);
+	expect(container.textContent).toMatch('apple');
+	expect(container.textContent).toMatch('orange');
+	expect(container.textContent).toMatch('pear');
+});
--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
-const ItemList = ( items ) => {
+const ItemList = ( { items } ) => {
+	console.log( 'items', items );
 	return items.length ? (
 		<ul>{items.map(i => <li key={i}>{i}</li>)}</ul>
 	) : (
